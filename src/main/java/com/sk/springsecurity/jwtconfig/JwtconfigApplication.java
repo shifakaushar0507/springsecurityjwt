bbcb09f10196cf84ae64c0553f6af74aff0e00d0
shifa kaushar <shifa.kaushar57@gmail.com>
2020-10-07 12:31:28 +0530

    Initial commit for spring-security-jwt

---
+++ src/main/java/com/sk/springsecurity/jwtconfig/JwtconfigApplication.java
@@ -0,0 +1,13 @@
+package com.sk.springsecurity.jwtconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtconfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtconfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sk/springsecurity/jwtconfig/JwtconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sk.springsecurity.jwtconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtconfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
